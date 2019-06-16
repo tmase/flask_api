8e745924d300d82c0ac3eca7ad5711831f17a71c
tmase <tommason1234@yahoo.com>
2019-06-15 21:52:51 -0400

    uppercase Flask import

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-import flask
+from flask import Flask
 from flask import request, jsonify
 import os
 import psycopg2
@@ -28,7 +28,7 @@ def api_filter():
 	
 	#submit sql query to database
 	conn = None
-	DATABASE_URL = os.environ['postgres://xguecwohsbjfdz:dcbaf4d0b37bf4f3a508cd044a7a56c7c6ef8c9d32b3668b4ac408b43bf6f086@ec2-23-21-91-183.compute-1.amazonaws.com:5432/d4lfl5vii49uqk']
+	DATABASE_URL = os.environ['DATABASE_URL']
 	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 	cur = conn.cursor()
 	cur.execute(sql)
