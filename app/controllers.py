360dc41fa81815649a0600697e6f16bf85307d34
tmase <tommason1234@yahoo.com>
2019-06-20 05:30:05 -0400

    add more routes

--- app/controllers.py
+++ app/controllers.py
@@ -1,5 +1,34 @@
 from app import app
+import os
+import psycopg2
+from flask import request, jsonify
+
 @app.route('/')
 @app.route('/index')
 def index():
 	return "hello, world"
+	
+@app.route('/api/v1/resources/summary', methods=['GET'])
+def api_filter():
+	sql = "SELECT * FROM summary WHERE"
+	query_parameters = request.args
+	for param in query_parameters:
+		val = query_parameters.get(param)
+		field_name = field_dict[param]
+		if param == 'start_date':
+			sql += " " + field_name + ">=" + val + "AND "
+		elif param == 'end_date':
+			sql += " " + field_name + "<=" + val + "AND "
+		else:
+			sql += " " + field_name + "=" + val + "AND "
+	sql = sql[:-4] + ';'
+	
+	#submit sql query to database
+	conn = None
+	DATABASE_URL = os.environ['DATABASE_URL']
+	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+	cur = conn.cursor()
+	cur.execute(sql)
+	results = cur.fetchall()
+	
+	return jsonify(results)
